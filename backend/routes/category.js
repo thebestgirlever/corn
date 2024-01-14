82386f75dc9a97f7685365037361969378b4d254
Mayya <maya1999@mail.ru>
2024-01-14 17:32:52 +0300

    add gh-pages

--- backend/routes/category.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var data = require('./data.json')
-
-router.get('/', function(req, res, next) {
-  res.json(data.categoryList);
-});
-
-module.exports = router;
